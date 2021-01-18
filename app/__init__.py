b0b9c5289d9a39e605bf58e445c81514cba223f3
Kaelyn Lia Kim <66226131+yklia@users.noreply.github.com>
2021-01-17 17:42:31 -0800

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,3 @@
-# export GOOGLE_APPLICATION_CREDENTIALS="C:\Users\Kim\Documents\hack_davis_backend\key.json"
-# export FLASK_APP=run.py
-
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
